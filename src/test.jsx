646d9daa880f50d390f6a69caf205b1bc5cac3ce
pmero <paulmero10@gmail.com>
2021-01-06 19:42:51 -0500

    base trabajo

---
+++ src/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const test = () => {
+    return (
+        <div>
+                <h1>Esto es una pruebas</h1>
+                <p>funcionado funcionando</p>
+        </div>
+    )
+}
+
+export default test
